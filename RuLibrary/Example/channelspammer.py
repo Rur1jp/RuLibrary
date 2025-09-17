c65414c744d9ab43e760168c4095de2125c053b2
るる <rurusama.77@gmail.com>
2025-09-17 20:56:27 +0900

    Delete RuLibrary/Example/channelspammer.py

--- RuLibrary/Example/channelspammer.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-from RuLibrary import discordSpam
-tokens = ["MTQ……","MTQ……"]
-channels = ["12345678910","123456789"]
-unko = "@everyone I am using this tool\nhttps://github.com/rur1jp/RuLibrary"
-kaisuu = 10
-asyncio.run(discordSpam(tokens, channels, msg=unko, repeat=kaisuu))
