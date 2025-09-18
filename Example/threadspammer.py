47eedefa16153c802a160b69d05b2f22cd48edf1
るる <rurusama.77@gmail.com>
2025-09-18 13:04:12 +0900

    Create threadspammer.py

---
+++ Example/threadspammer.py
@@ -0,0 +1,9 @@
+from RuLibrary import thread
+
+tokens = ["MTQ……","MTQ……"]
+channelid = "123456789"
+name = "a"
+kaisuu = 3
+kankaku2 = 0.5
+
+thread(tokens, channelid, name, count=kaisuu, kankaku=kankaku2)
